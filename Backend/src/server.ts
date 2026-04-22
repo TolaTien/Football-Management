7a14941815a271b0557ba11550de404a0fea057b
TolaTien <tien0bip009@gmail.com>
2026-04-22 17:06:00 +0700

    Init Backend

---
+++ Backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import type { Request, Response } from "express";
+
+const PORT = 3000;
+const app = express()
+app.use(express.json({ limit: "10mb" }));
+
+app.get('/', (req: Request, res: Response) =>{
+    res.send("Hello PTIT")
+}) 
+
+app.listen(PORT, "0.0.0.0", () =>{
+    console.log(`Server is running in ${PORT}`);
+} )
