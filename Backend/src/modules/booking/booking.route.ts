1126fae4adaabf9d62a5d3664c3869d8251befcf
TolaTien <tien0bip009@gmail.com>
2026-05-02 09:36:49 +0700

    feat: add API bookPitchForUser + partialPayment

---
+++ Backend/src/modules/booking/booking.route.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { authUser } from "../../middlewares/auth.middleware.js";
+import Booking from './booking.controller.js';
+
+export const bookingRouters: Router = Router();
+
+bookingRouters.post('/booking-pitch-user', authUser, Booking.bookPitchForUser);
+bookingRouters.post('/payment-user', authUser, Booking.partialPayment);
--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -2,9 +2,11 @@ import { Router } from "express";
 import { authRouters } from "../modules/auth/auth.route.js";
 import { userRouters } from "../modules/users/users.route.js";
 import { pitchRouters } from "../modules/pitch/pitch.route.js";
+import { bookingRouters } from "../modules/booking/booking.route.js";
 
 export const Routers: Router = Router();
 
 Routers.use('/auth', authRouters);
 Routers.use('/user', userRouters);
 Routers.use('/pitch', pitchRouters);
+Routers.use('/booking', bookingRouters);
