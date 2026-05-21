95d95710a6a9697a2af9c7a74cb6e7720efd765e
datntptit24-source <datntptit24@gmail.com>
2026-05-21 23:38:41 +0700

    fix:remove zod

--- Backend/src/modules/comments/comments.route.ts
+++ Backend/src/modules/comments/comments.route.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import { CommentController } from "./comments.controller.js";
 import { authUser } from "../../middlewares/auth.middleware.js";
-import { validate } from "../../middlewares/validate.middleware.js";
-import { CommentsSchema } from "./comments.schema.js";
 
 export const commentRouters: Router = Router();
-commentRouters.get("/post/:postId", authUser, validate(CommentsSchema.paramsPostId), CommentController.getByPost);
-commentRouters.post("/", authUser, validate(CommentsSchema.create), CommentController.create);
-commentRouters.post("/:commentId/like", authUser, validate(CommentsSchema.paramsCommentId), CommentController.toggleLike);
+commentRouters.get("/post/:postId", authUser, CommentController.getByPost);
+commentRouters.post("/", authUser, CommentController.create);
+commentRouters.post("/:commentId/like", authUser, CommentController.toggleLike);
--- Backend/src/modules/postlikes/postlikes.route.ts
+++ Backend/src/modules/postlikes/postlikes.route.ts
@@ -1,8 +1,7 @@
 import { Router } from "express";
 import { PostLikeController } from "./postlikes.controller.js";
 import { authUser } from "../../middlewares/auth.middleware.js";
-import { validate } from "../../middlewares/validate.middleware.js";
-import { PostLikesSchema } from "./postlikes.schema.js";
+
 
 export const postLikeRouters: Router = Router();
-postLikeRouters.post("/:postId/like", authUser, validate(PostLikesSchema.toggleLike), PostLikeController.toggleLike);
+postLikeRouters.post("/:postId/like", authUser,  PostLikeController.toggleLike);
