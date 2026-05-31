ebbe0073f61634bd2f37e743cf30f73e272592bc
TolaTien <tien0bip009@gmail.com>
2026-05-31 09:40:22 +0700

    fix: improve token handling in auth middleware and update cookie options for production

--- Backend/src/middlewares/auth.middleware.ts
+++ Backend/src/middlewares/auth.middleware.ts
@@ -6,7 +6,12 @@ import { prisma } from "../config/prisma.js";
 
 export const authUser = async (req: Request, res: Response, next: NextFunction) => {
     try{
-        const token = req.cookies?.accessToken;
+        let token = req.cookies?.accessToken;
+
+        if (!token && req.headers.authorization?.startsWith('Bearer ')) {
+            token = req.headers.authorization.split(' ')[1];
+        }
+
         if(!token) {
             return res.status(401).json({ message: "Vui lòng đăng nhập"})
         }
